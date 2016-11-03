5c21e841a0f0b7ae088085e006e402fea33fc924
Chad Homan <choman@gmail.com>
2016-11-03 12:56:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+This is the client desktop for the "new" system
+
+
+clone this repo and run the install
+NNOTE: you may need to install git first
+
+
+#
+# Install/Clone Commands
+sudo apt update && sudo apt install -y git
+git clone https://github.com/choman/clientdesktop.git
+
+
+
---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+#
+# Add ppas
+PPAS=(
+   "ppa:libreoffice/ppa"
+   "ppa:saiarcot895/myppa"
+  )
+
+
+for i in ${PPAS[@]}; do
+    sudo apt-add-repository -y $i
+done
+
+# setup chrome ppa
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+
+#
+# install apt-fast
+sudo apt update
+sudo apt install -y apt-fast di axel 
+sudo apt-fast dist-upgrade -y
+
+#
+# determine vbox version
+#   - place holder
+
+#
+# install extra software, ppas should be installed above.  see PPAS
+#   - chrome
+#   - tor
+#   - desktop recorders
+sudo apt-fast install -y google-chrome-stable
+
+
+#
+# Setup chrome to run android apps
+
+
