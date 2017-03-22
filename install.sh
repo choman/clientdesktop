04b96154fa4e48025b47bdde78dc2f6eca3617fa
chad <choman@gmail.com>
2017-03-22 00:26:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,11 +17,13 @@ KEYSFILE=keys
 TOKENFILE=token
 
 function setup_vault() {
-    if [ ! -f /usr/local/bin/vault ]; then 
-        wget -nc -O /tmp/vault.zip https://releases.hashicorp.com/vault/0.7.0/vault_0.7.0_linux_amd64.zip
-        unzip /tmp/vault.zip -d /tmp
-        sudo cp /tmp/vault /usr/local/bin
+    vers=0.7.0
+
+    if [ ! -f /usr/local/bin/vault ]; then
+        wget -nc -O /tmp/vault.zip https://releases.hashicorp.com/vault/${vers}/vault_${vers}_linux_amd64.zip
+        sudo unzip /tmp/vault.zip -d /usr/local/bin
     fi
+
 }
 
 function parse_yaml2() {
